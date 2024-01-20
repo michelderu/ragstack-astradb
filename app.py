cdc0789e358b330a520a6ff592dea3e77ac3d239
Mangat Rai <mangatrai@gmail.com>
2024-01-19 23:45:21 -0600

    made logo configurable for customer demo

--- app.py
+++ app.py
@@ -380,10 +380,16 @@ try:
 except:
     st.markdown(Path('welcome.md').read_text())
 
+logo_file_name = './assets/' + username + '-logo.svg'
+
 # DataStax logo
 with st.sidebar:
-    st.image('./assets/uniphore-logo.svg', use_column_width="always")
-    st.text('')
+    try:
+        st.image(logo_file_name, use_column_width="always")
+        st.text('')
+    except:
+        st.image('./assets/datastax-logo.svg', use_column_width="always")
+        st.text('')
 
 # Logout button
 with st.sidebar:
